26f11afb540fac7149a5c41b1f7e668b76ed3253
Dave Olsen <dmolsen@gmail.com>
2014-06-22 17:00:41 -0400

    support for mustache helpers

--- src/PatternLab/PatternEngine/Rules/MustacheRule.php
+++ src/PatternLab/PatternEngine/Rules/MustacheRule.php
@@ -18,6 +18,8 @@ use \PatternLab\PatternEngine\Loaders\MustacheLoader;
 
 class MustacheRule extends \PatternLab\PatternEngine\Rule {
 	
+	protected $helpers = array();
+	
 	public function __construct($options) {
 		
 		parent::__construct($options);
@@ -28,10 +30,38 @@ class MustacheRule extends \PatternLab\PatternEngine\Rule {
 		
 	public function getInstance($options) {
 		
+		$this->loadHelpers();
+		
 		$options["loader"]         = new MustacheLoader(__DIR__."/../../".Config::$options["patternSourceDir"],array("patternPaths" => $options["patternPaths"]));
 		$options["partial_loader"] = new MustacheLoader(__DIR__."/../../".Config::$options["patternSourceDir"],array("patternPaths" => $options["patternPaths"]));
+		$options["helpers"]        = $this->helpers;
 		
 		return new \Mustache_Engine($options);
+		
+	}
+	
+	/**
+	* Load helpers to add tags to Mustache
+	*/
+	protected function loadHelpers() {
+		
+		$objects = new \RecursiveIteratorIterator(new \RecursiveDirectoryIterator(Config::$options["pluginDir"]), \RecursiveIteratorIterator::CHILD_FIRST);
+		
+		// make sure dots are skipped
+		$objects->setFlags(\FilesystemIterator::SKIP_DOTS);
+		
+		foreach($objects as $name => $object) {
+			
+			if ((strpos($name,"MustacheHelper.php") !== false) && (strpos($name,"plugins/vendor/") === false)) {
+				$dirs            = explode("/",$object->getPath());
+				$helperName      = "\\".$dirs[count($dirs)-2]."\\".$dirs[count($dirs)-1]."\\".str_replace(".php","",$object->getFilename());
+				$helper          = new $helperName();
+				foreach ($helper->helpers as $tag => $function) {
+					$this->helpers[$tag] = $function;
+				}
+			}
+			
+		}
 		
 	}
 	
