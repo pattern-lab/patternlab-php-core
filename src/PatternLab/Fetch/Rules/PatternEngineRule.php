fb676310cef32e84e139b21a952d935758a8bb0b
Dave Olsen <dmolsen@gmail.com>
2014-06-22 20:17:26 -0400

    fetch rule to grab pattern engines

---
+++ src/PatternLab/Fetch/Rules/PatternEngineRule.php
@@ -0,0 +1,33 @@
+<?php
+
+/*!
+ * Pattern Engine Fetch Rule Class
+ *
+ * Copyright (c) 2014 Dave Olsen, http://dmolsen.com
+ * Licensed under the MIT license
+ *
+ * How to handle requests for plug-ins
+ *
+ */
+
+namespace PatternLab\Fetch\Rules;
+
+use \PatternLab\Config;
+use \PatternLab\Fetch\Rule;
+
+class PatternEngineRule extends Rule {
+	
+	public function __construct() {
+		
+		parent::__construct();
+		
+		$this->name          = "pattern engine";
+		$this->unpack        = false;
+		$this->writeDir      = Config::$options["pluginDir"];
+		
+		$this->shortCommand  = "e";
+		$this->longCommand   = "patternengine";
+		
+	}
+	
+}
