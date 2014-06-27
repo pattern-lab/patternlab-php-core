3328532e1099b5fb45a28f172ff1fe69bc3f8666
Dave Olsen <dmolsen@gmail.com>
2014-06-27 16:56:11 -0400

    find the base templates in the styleguidekit

--- src/PatternLab/Template/Loader.php
+++ src/PatternLab/Template/Loader.php
@@ -12,6 +12,8 @@
 
 namespace PatternLab\Template;
 
+use \PatternLab\Config;
+
 class Loader {
 	
 	/**
@@ -20,9 +22,10 @@ class Loader {
 	* @return {Object}       an instance of the Mustache engine
 	*/
 	public static function fileSystem() {
+		$templatePath = Config::$options["pluginDir"]."/".Config::$options["styleguideKit"]."/views";
 		return new \Mustache_Engine(array(
-						"loader" => new \Mustache_Loader_FilesystemLoader(__DIR__."/../../../templates/"),
-						"partials_loader" => new \Mustache_Loader_FilesystemLoader(__DIR__."/../../../templates/partials/")
+						"loader" => new \Mustache_Loader_FilesystemLoader($templatePath),
+						"partials_loader" => new \Mustache_Loader_FilesystemLoader($templatePath."/partials")
 		));
 	}
 	
