59f1da93a58006baa2a30e8bf1549c44bdc95121
Dave Olsen <dmolsen@gmail.com>
2014-07-09 10:42:36 -0400

    adding a function to normalize/clean paths

--- src/PatternLab/FileUtil.php
+++ src/PatternLab/FileUtil.php
@@ -59,4 +59,44 @@ class FileUtil {
 		return false;
 	}
 	
+	/**
+	* Taken from Composer: https://github.com/composer/composer/blob/master/src/Composer/Util/Filesystem.php
+	*
+	* Normalize a path. This replaces backslashes with slashes, removes ending
+	* slash and collapses redundant separators and up-level references.
+	*
+	* @param  string $path Path to the file or directory
+	* @return string
+	*/
+	public static function normalizePath($path) {
+		$parts = array();
+		$path = strtr($path, '\\', '/');
+		$prefix = '';
+		$absolute = false;
+		
+		if (preg_match('{^([0-9a-z]+:(?://(?:[a-z]:)?)?)}i', $path, $match)) {
+			$prefix = $match[1];
+			$path = substr($path, strlen($prefix));
+		}
+		
+		if (substr($path, 0, 1) === '/') {
+			$absolute = true;
+			$path = substr($path, 1);
+		}
+		
+		$up = false;
+		foreach (explode('/', $path) as $chunk) {
+			if ('..' === $chunk && ($absolute || $up)) {
+				array_pop($parts);
+				$up = !(empty($parts) || '..' === end($parts));
+			} elseif ('.' !== $chunk && '' !== $chunk) {
+				$parts[] = $chunk;
+				$up = '..' !== $chunk;
+			}
+		}
+		
+		return $prefix.($absolute ? '/' : '').implode('/', $parts);
+		
+	}
+	
 }
