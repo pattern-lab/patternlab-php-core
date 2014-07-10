3fc150581eca2ef18307529e7304dc42b2a084fd
Dave Olsen <dmolsen@gmail.com>
2014-07-10 16:16:22 -0400

    only load from vendor in the dir in the root of the project

--- builder.php
+++ builder.php
@@ -15,15 +15,15 @@ if (!function_exists("json_decode")) {
 }
 
 // auto-load classes
-require(__DIR__."/vendor/autoload.php");
+if (file_exists(__DIR__."/../vendor/autoload.php")) {
+	require(__DIR__."/../vendor/autoload.php");
+} else {
+	print "vendor should exist...\n";
+	exit;
+}
 
 // load the options
 \PatternLab\Config::init();
-
-// autoload plugin vendored items if available
-if (file_exists(__DIR__."/../vendor/autoload.php")) {
-	require(__DIR__."/../vendor/autoload.php");
-}
 
 // initialize the dispatcher & note that the config has been loaded
 \PatternLab\Dispatcher::init();
