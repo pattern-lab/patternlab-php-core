0d411d076495ee232d3c2604e414d9d36544a140
Dave Olsen <dmolsen@gmail.com>
2014-07-17 13:45:04 -0400

    removing unused commands

--- src/PatternLab/Console/Commands/_BuildCommand.php
+++
@@ -1,2 +0,0 @@
-// alias for generate so have it "extend"?
-$console->setCommand("b","build","Alias for the generate command","Alias for the generate command. Please refer to it's help for full options.");
