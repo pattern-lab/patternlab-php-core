d4bebf823d98d8fe079cc880a67f85881ea4744c
Dave Olsen <dmolsen@gmail.com>
2015-02-28 14:48:08 -0500

    adding the starterkit command

---
+++ src/PatternLab/Console/Commands/StarterKitCommand.php
@@ -0,0 +1,58 @@
+<?php
+
+/*!
+ * Console StarterKit Command Class
+ *
+ * Copyright (c) 2014 Dave Olsen, http://dmolsen.com
+ * Licensed under the MIT license
+ *
+ */
+
+namespace PatternLab\Console\Commands;
+
+use \PatternLab\Config;
+use \PatternLab\Console;
+use \PatternLab\Console\Command;
+use \PatternLab\Fetch;
+use \PatternLab\Timer;
+
+class StarterKitCommand extends Command {
+	
+	public function __construct() {
+		
+		parent::__construct();
+		
+		$this->command = "starterkit";
+		
+		Console::setCommand($this->command,"Initialize or fetch a specific StarterKit","The StarterKit command downloads StarterKits.","k");
+		Console::setCommandOption($this->command,"init","Initialize with a blank StarterKit based on the active PatternEngine.","To initialize your project with a base StarterKit:","i");
+		Console::setCommandOption($this->command,"fetch:","Fetch a specific StarterKit from GitHub.","To fetch a StarterKit from GitHub:","f:","<starterkit-name>");
+		
+	}
+	
+	public function run() {
+		
+		// find the value given to the command
+		$init       = Console::findCommandValue("i|init");
+		$starterkit = Console::findCommandValue("f|fetch");
+		
+		if ($init) {
+			$patternEngine = Config::getOption("patternExtension");
+			$starterkit    = "pattern-lab/starterkit-".$patternEngine."-base";
+		}
+		
+		if ($starterkit) {
+			
+			// download the starterkit
+			$f = new Fetch();
+			$f->fetchStarterKit($starterkit);
+			
+		} else {
+			
+			Console::writeHelpCommand($this->command);
+			
+		}
+		
+	}
+	
+}
