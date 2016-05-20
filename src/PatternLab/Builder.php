51ba7b11d1003d4a083f6fe6cc9b585f6a86f0d9
Dave Olsen <dmolsen@gmail.com>
2016-05-20 16:14:37 -0400

    removing encoded versions of files

--- src/PatternLab/Builder.php
+++ src/PatternLab/Builder.php
@@ -243,9 +243,8 @@ class Builder {
 				
 				// modify the pattern mark-up
 				$markup        = $patternStoreData["code"];
-				$markupEncoded = htmlentities($markup,ENT_COMPAT,"UTF-8");
 				$markupFull    = $patternStoreData["header"].$markup.$patternStoreData["footer"];
-				$markupEngine  = htmlentities(file_get_contents($patternSourceDir."/".$pathName.".".$patternExtension),ENT_COMPAT,"UTF-8");
+				$markupEngine  = file_get_contents($patternSourceDir."/".$pathName.".".$patternExtension);
 				
 				// if the pattern directory doesn't exist create it
 				if (!is_dir($patternPublicDir."/".$path)) {
@@ -255,7 +254,7 @@ class Builder {
 				// write out the various pattern files
 				file_put_contents($patternPublicDir."/".$path."/".$path.".html",$markupFull);
 				if (!$exportFiles) {
-					file_put_contents($patternPublicDir."/".$path."/".$path.".escaped.html",$markupEncoded);
+					file_put_contents($patternPublicDir."/".$path."/".$path.".markup-only.html",$markup);
 					file_put_contents($patternPublicDir."/".$path."/".$path.".".$patternExtension,$markupEngine);
 				}
 				/*
