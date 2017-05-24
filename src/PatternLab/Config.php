8a51a732714a8e4809e62b5deba5618c5f1e9087
Evan Lovely <evanlovely@gmail.com>
2017-05-24 11:50:13 -0700

    updating config path to use our vendor name

--- src/PatternLab/Config.php
+++ src/PatternLab/Config.php
@@ -129,7 +129,8 @@ class Config {
 		self::$userConfigDirDash   = self::$options["baseDir"].self::$userConfigDirDash;
 		self::$userConfigDir       = (is_dir(self::$userConfigDirDash)) ? self::$userConfigDirDash : self::$userConfigDirClean;
 		self::$userConfigPath      = self::$userConfigDir.DIRECTORY_SEPARATOR.self::$userConfig;
-		self::$plConfigPath        = self::$options["baseDir"]."vendor/pattern-lab/core/".self::$plConfigPath;
+		// @todo Make folder name (i.e. `drupal-pattern-lab`) a variable
+		self::$plConfigPath        = self::$options["baseDir"]."vendor/drupal-pattern-lab/core/".self::$plConfigPath;
 		
 		// can't add __DIR__ above so adding here
 		if (!is_dir(self::$userConfigDir)) {
